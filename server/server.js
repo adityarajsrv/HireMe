3df3cf4c0060ae31d9fbb1814722ea07f39a0160
Aditya <adityarajsrvofficial@gmail.com>
2025-09-28 01:33:26 +0530

    Fixing production bugs

--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config();
 const connectDB = require('./config/db');
-connectDB();
-
 const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
 
+dotenv.config();
+connectDB();
+
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://hire-me-inky.vercel.app', 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
